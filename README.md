5f74afb03a2f9722be74db8b055747934989a93f
NatyNatur <naticcg@gmail.com>
2017-11-12 18:52:33 -0300

    versión 0.0.1

---
+++ README.md
@@ -0,0 +1,7 @@
+# Reto de código: Trello
+`Versión 0.0.1`
+
+
+## Contenido: 
+* Se muestra el texto "Añadir una lista ...", que al hacer click se debe texto y muestra un formulario.
+* El formulario está conformado por un input y un botón para que pueda añadir tareas a una lista.
--- assets/js/app.js
+++ assets/js/app.js
@@ -1,10 +1,17 @@
-var counter = 1;
+//Crear variables globales
 var container = document.getElementById('container');
 var btn = document.getElementById('btn');
 var task = document.getElementById('newTask');
 var taskList = document.getElementById('tasksList');
-var newTasks = document.getElementById('tasks');
-var form = document.getElementById('form');
-form.style.display = 'none';
-btn.addEventListener('click', addTask);
+var newTasks = document.getElementById('newList');
+var forms = document.getElementById('forms');
+var listToHide = document.getElementById('listToHide');
+
+//evento en el que al hacer click en el primer input, este se oculta y aparece el formulario
+listToHide.addEventListener('click', function () {
+	listToHide.style.display = 'none';
+	forms.style.display = 'inline-block';
+});
+
+
 
