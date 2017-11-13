e550afa8a7a2257cf48f29bc8d7bd98e8c285aad
NatyNatur <naticcg@gmail.com>
2017-11-12 22:54:18 -0300

    casi versión 0.0.2 WHY?!

--- README.md
+++ README.md
@@ -1,7 +1,13 @@
 # Reto de código: Trello
+
+
 `Versión 0.0.1`
-
-
 ## Contenido: 
-* Se muestra el texto "Añadir una lista ...", que al hacer click se debe texto y muestra un formulario.
-* El formulario está conformado por un input y un botón para que pueda añadir tareas a una lista.
+* Se muestra el texto "Añadir una lista ...", que al hacer click, desaparece el texto y muestra un formulario.
+* El formulario está conformado por un input y un botón para que pueda añadir el nombre de una lista.
+
+
+`Versión 0.0.2`
+## Contenido: 
+* Al dar click en el botón de "Guardar", se mostrará un nuevo cuadro donde estará el nombre de la lista agregada. *parcialmente logrado, "lo nuevo" no se queda (WHY?!), trabajando en ello...*
+* Mostrar un texto de "Añadir una tarea" dentro de la lista.
--- assets/js/app.js
+++ assets/js/app.js
@@ -1,9 +1,9 @@
 //Crear variables globales
 var container = document.getElementById('container');
+var newContainer = document.getElementById('newContainer');
 var btn = document.getElementById('btn');
-var task = document.getElementById('newTask');
+var list = document.getElementById('newList');
 var taskList = document.getElementById('tasksList');
-var newTasks = document.getElementById('newList');
 var forms = document.getElementById('forms');
 var listToHide = document.getElementById('listToHide');
 
@@ -13,5 +13,33 @@ listToHide.addEventListener('click', function () {
 	forms.style.display = 'inline-block';
 });
 
+//evento en que al hacer click en el botón, guarda el nombre de lista
+btn.addEventListener('click', function () {
+	//obtener lista ingresada en el input
+	var list = document.getElementById('newList').value;
+	//limpiar el text area
+	list.value = '';
+	//crear div que contenga la lista
+	var tasksList = document.createElement('div');
+	//crear título
+	var newListName = document.createElement('p');
+	//dándole id al nombre de la lista
+	newListName.setAttribute('id','listName');
+	//creando nodo de texto
+	var listContent = document.createTextNode(list);
+	//creando nuevo link para nuevas tareas, con sus atributos
+	var nextStep = document.createElement('a');
+	nextStep.setAttribute('href','#');
+	nextStep.setAttribute('id','nextStep');
+	var nextStepText = document.createTextNode('Añadir una tarjeta');
+	//appencheando, jiji
+	newListName.appendChild(listContent);
+	nextStep.appendChild(nextStepText);
+	taskList.appendChild(newListName);
+	// dándole id para dar estilos
+	taskList.setAttribute('id', 'newTaskList');
+	taskList.appendChild(nextStep);
+	newContainer.appendChild(taskList);
 
+});
 
